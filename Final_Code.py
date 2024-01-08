7ce6f55df7c215d25a5cbbd6010e81181d116459
XenosHu <121327330+XenosHu@users.noreply.github.com>
2024-01-08 02:04:06 -0500

    Update Final_Code.py

--- Final_Code.py
+++ Final_Code.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
 
 import streamlit as st
 import yfinance as yf
@@ -15,9 +13,6 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-
-
-# In[7]:
 
 
 # Fetch the list of S&P 500 components from Wikipedia
@@ -54,17 +49,11 @@ bse_sensex_data = pd.read_html(url5)[1]
 bse_sensex_tickers = bse_sensex_data['Symbol'].tolist()
 
 
-# In[8]:
-
-
 # List of available indexes
 available_indexes = ["S&P 500", "NASDAQ 100", "DOWJONES", "FTSE 100", "BSE SENSEX"]
 
 # Sidebar: Index Selection
 index_selection = st.sidebar.selectbox("Select an Index", available_indexes)
-
-
-# In[9]:
 
 
 index_mapping = {
@@ -76,13 +65,7 @@ index_mapping = {
 }
 
 
-# In[10]:
-
-
 index_symbol = index_mapping.get(index_selection)
-
-
-# In[11]:
 
 
 @st.cache_data
@@ -115,19 +98,11 @@ def get_stock_list(index_name):
     return stock_list
 
 
-# In[12]:
-
 
 ticker_list = get_stock_list(index_selection)
 
 
-# In[13]:
-
-
 st.markdown("<h1 style='text-align: center;'>Stock Market Analysis Dashboard</h1>", unsafe_allow_html=True)
-
-
-# In[14]:
 
 
 @st.cache_data
@@ -136,8 +111,6 @@ def get_stock_data(stock_symbol, start_date, end_date):
     stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
     return stock_data
 
-
-# In[15]:
 
 
 @st.cache_data
@@ -167,9 +140,6 @@ def get_index_data(index_symbol, timeframe):
     return index_data
 
 
-# In[16]:
-
-
 @st.cache_data
 def get_currency(stock_symbol):
     stock_data_other = yf.Ticker(stock_symbol)
@@ -178,13 +148,7 @@ def get_currency(stock_symbol):
     return info,currency
 
 
-# In[17]:
-
-
 datetime.today()
-
-
-# In[18]:
 
 
 st.sidebar.title("Select Parameters")
@@ -194,8 +158,6 @@ default_start_date = datetime.today() - timedelta(weeks=52)
 start_date = st.sidebar.date_input("Start Date", default_start_date)
 end_date = st.sidebar.date_input("End Date")
 
-
-# In[19]:
 
 
 if start_date >= end_date:
@@ -221,33 +183,6 @@ else:
                       xaxis_title='Date')
 
     st.plotly_chart(fig)
-
-
-# In[20]:
-
-
-warning_text = "<span style='color:red;'>Bias Warning</span>"
-
-# Display the warning text
-st.markdown(warning_text, unsafe_allow_html=True)
-
-with st.expander("About Confirmation Bias"):
-    st.write("""
-    **Beware of Confirmation Bias:** 🧠
-
-    Confirmation bias is a powerful psychological tendency where we unconsciously seek out information that supports our existing beliefs. In the world of investing, this means you might be tempted to choose a timeframe that aligns with your preconceived notions about a stock.
-
-    📈 If you're feeling bullish, you might prefer shorter timeframes, hoping to find recent price increases.
-    📉 If you're bearish, you might lean towards longer timeframes, seeking evidence of long-term declines.
-
-    But remember, true investment wisdom comes from an objective assessment of all available data. Don't fall into the trap of cherry-picking data to support your outlook. Consider various timeframes to make well-informed decisions. 📊💼
-
-    Stay rational and keep confirmation bias at bay!
-    """)
-
-
-# In[21]:
-
 
 st.subheader(f"{other_data[0]} Stock Summary")
 if not stock_data.empty:
@@ -281,13 +216,8 @@ else:
     st.write("Stock data is not available. Please select a valid stock.")
 
 
-# In[22]:
-
-
 pricing_data, fundamental_data, news  = st.tabs(["Pricing Data", "Fundamental Data", "Top News"])
 
-
-# In[23]:
 
 
 with pricing_data:
@@ -338,13 +268,7 @@ with pricing_data:
     st.plotly_chart(fig)
 
 
-# In[24]:
-
-
 analyzer = SentimentIntensityAnalyzer()
-
-
-# In[25]:
 
 
 @st.cache_data
@@ -370,33 +294,11 @@ def print_stock_news(stock_symbol):
     return top_news
 
 
-# In[26]:
-
-
 if selected_stock:
     top_5_news = print_stock_news(selected_stock)
 
 
-# In[27]:
-
-
 with news:
-    
-    warning_text = "<span style='color:red;'>Bias Warning</span>"
-
-    # Display the warning text
-    st.markdown(warning_text, unsafe_allow_html=True)
-
-    with st.expander("About Anchoring Bias"):
-        st.write("""
-        **Anchoring Bias Alert:** 🧠
-
-        When you encounter sentiment analysis, especially if it's the first analysis you see, it can set an initial reference point or 'anchor' for your expectations. Subsequent sentiment analyses or news might then be evaluated in relation to this anchor.
-
-        For example, if you read a highly positive sentiment analysis initially, you may anchor your expectations to that positivity. Later news, even if it's still positive but not as optimistic, could be perceived as disappointing. This bias can influence your investment decisions.
-
-        Stay mindful of your anchors, and consider each piece of information independently to make informed decisions. Don't let an early anchor overly influence your judgment.
-        """)
     st.subheader(f'Top News for {selected_stock}')
     for i, news_item in enumerate(top_5_news):
 
@@ -409,11 +311,6 @@ with news:
 
         st.write("Sentiment Score:", f"<font color='{sentiment_color}'>{sentiment_score}</font>", unsafe_allow_html=True)
         
-        
-
-
-# In[28]:
-
 
 @st.cache_data
 def get_fundamental_metrics(stock_symbol):
@@ -433,9 +330,6 @@ def get_fundamental_metrics(stock_symbol):
     return fundamental_metrics
 
 
-# In[29]:
-
-
 with fundamental_data:
     
     with st.expander("Definitions of Fundamental Data"):
@@ -452,14 +346,7 @@ with fundamental_data:
     fundamental_metrics = get_fundamental_metrics(selected_stock)
 
     for metric, value in fundamental_metrics.items():
-        st.write(f"{metric}: {value}")
-    
-    
-    
-
-
-# In[30]:
-
+        st.write(f"{metric}: {value}")   
 
 st.header("Stock Price Comparison")
 
@@ -498,10 +385,6 @@ else:
 
     st.plotly_chart(fig)    
 
-
-# In[31]:
-
-
 st.header(f"{index_selection} Index Performance")
 
 index_timeframe = ['5 Day', '1 Week', '1 Month', '6 Months', 'YTD', '1 Year', '5 Year']
@@ -528,10 +411,6 @@ else:
 
     # Display the chart in the Streamlit app
     st.plotly_chart(fig)
-
-
-# In[72]:
-
 
 def fetch_market_cap_data(index_tickers):
     market_cap_data = {}
@@ -563,39 +442,16 @@ def fetch_market_cap_data(index_tickers):
     return merged_df
 
 
-# In[57]:
-
-
 new_tickerlist_nasdaq = ['AAPL','MSFT', 'GOOG', 'GOOGL', 'AMZN', 'NVDA', 'META', 'TSLA', 'AVGO', 'ASML']
-
-
-# In[58]:
-
 
 new_tickerlist_sp500 = ['AAPL', 'MSFT', 'GOOG', 'GOOGL', 'AMZN', 'NVDA', 'META', 'TSLA', 'LLY', 'V']
 
-
-# In[59]:
-
-
 new_tickerlist_dowjones = ['AAPL', 'MSFT', 'V', 'UNH', 'JNJ', 'JPM', 'WMT', 'PG', 'HD', 'CVX']
-
-
-# In[60]:
-
 
 new_tickerlist_ftse100 = ['AZN', 'SHEL', 'BA', 'BP', 'RIO', 'GSK', 'JD', 'ADM', 'CRH', 'HLN']
 
-
-# In[61]:
-
-
 new_tickerlist_bse = ['RELIANCE.BO', 'TCS.BO', 'HDFCBANK.BO', 'ICICIBANK.BO', 'HINDUNILVR.BO', 'INFY.BO', 'ITC.BO', 'SBIN.BO',
                       'BHARTIARTL.BO', 'BAJFINANCE.BO']
-
-
-# In[73]:
-
 
 
 if index_selection == "S&P 500":
@@ -626,10 +482,6 @@ elif index_selection == "BSE SENSEX":
 else:
     ticker_list_2 = []
 
-
-# In[75]:
-
-
 st.header(f'Top Stocks by Market Cap - {index_selection}')
 # Plot a treemap using Plotly Express
 fig = px.treemap(top_10_stocks, path= ['Ticker'], values='MarketCap',
@@ -637,35 +489,7 @@ fig = px.treemap(top_10_stocks, path= ['Ticker'], values='MarketCap',
 
 st.plotly_chart(fig)
 
-
-# In[40]:
-
-
-warning_text = "<span style='color:red;'>Bias Warning</span>"
-
-# Display the warning text
-st.markdown(warning_text, unsafe_allow_html=True)
-
-with st.expander("About Herd Mentality"):
-    st.write("""
-    **Herd Mentality Alert:** 🐑
-
-    Herd mentality is a powerful psychological force that can influence investment decisions. When you see the largest market cap stocks in the Treemap, there might be a temptation to follow the crowd and invest in these stocks. They are often seen as safer and more reliable due to their size.
-
-    However, it's essential to remember that what works for the herd may not always be the best choice for your unique financial goals and risk tolerance. Investment decisions should be based on a careful assessment of your own objectives and research, rather than blindly following the crowd.
-
-    Stay independent in your choices and consider a diversified approach. This can help you avoid the pitfalls of herd mentality and make decisions that align with your personal financial strategy.
-""")
-
-
-# In[41]:
-
-
 index_data = fetch_market_cap_data(ticker_list_2)
-
-
-# In[79]:
-
 
 
 sector_performance = index_data.groupby('Sector')['MarketCap'].sum().reset_index()
@@ -679,9 +503,6 @@ fig = px.bar(sector_performance, x='Sector', y='MarketCap',
 fig.update_xaxes(categoryorder='total descending')
 
 st.plotly_chart(fig)
-
-
-# In[43]:
 
 
 def plot_sma_vs_closing_price(stock_symbol, start, end):
@@ -709,9 +530,6 @@ def plot_sma_vs_closing_price(stock_symbol, start, end):
     st.plotly_chart(fig)
 
 
-# In[44]:
-
-
 def plot_ema_vs_closing_price(stock_symbol, start, end):
     # Retrieve stock data using yfinance
     stock_data = yf.download(stock_symbol, start=start, end=end)
@@ -736,137 +554,12 @@ def plot_ema_vs_closing_price(stock_symbol, start, end):
     # Display the Plotly figure in Streamlit
     st.plotly_chart(fig)
 
-
-# In[45]:
-
-
 st.header("Trend Analysis using Indicators")
 indicator_type = st.selectbox("Select Indicator Type", ["sma", "ema"])
-                                               
-
-
-# In[46]:
-
 
 if indicator_type == "sma":
     sma_plot = plot_sma_vs_closing_price(selected_stock, start_date, end_date)
     
 elif indicator_type == 'ema':
     ema_plot = plot_ema_vs_closing_price(selected_stock, start_date, end_date)       
-
-
-# ## Simulation
-
-# In[47]:
-
-
-def monte_carlo_simulation(returns, initial_investment, num_years, num_simulations):
-    portfolio_values = np.zeros((num_simulations, num_years * 252))
-    for i in range(num_simulations):
-        # Convert returns to DataFrame for random sampling
-        returns_df = returns.to_frame()
-        sampled_returns = returns_df.sample(num_years * 252, replace=True, axis=0)
-        price_paths = initial_investment * np.cumprod(1 + sampled_returns)
-        portfolio_values[i, :] = np.sum(price_paths, axis=1)
-    return portfolio_values
-
-
-# In[82]:
-
-
-st.header("Stock Portfolio Monte Carlo Simulation")
-st.write('''Monte Carlo Simulation gives a wide range of potential future outcomes by running a large number of random simulations.
-            Provides valuable insights into risk and uncertainty.''')
-
-initial_investment = st.number_input("Initial Portfolio Value:", min_value=1000, value=10000)
-num_years = st.number_input("Number of Simulation Years:", min_value=1, value=5)
-num_simulations = st.number_input("Number of Simulations:", min_value=1, value=10)
-
-st.write("To run the Monte Carlo Simulation, simply adjust the input parameters above (Initial Portfolio Value, Number of Simulation Years, Number of Simulations), and then click the 'Run Simulation' button below.")
-
-
-# In[49]:
-
-
-stock_returns = stock_data['Adj Close'].pct_change().dropna()
-
-
-# In[50]:
-
-
-simulation_results = monte_carlo_simulation(stock_returns, initial_investment, num_years, num_simulations)
-
-
-# In[51]:
-
-
-fig_simulation = go.Figure()
-
-for i in range(num_simulations):
-    # Convert the range object to a list using the list() function
-    x_values = list(range(num_years * 252))
-    fig_simulation.add_trace(go.Scatter(x=x_values, y=simulation_results[i, :], mode='lines', name=f'Simulation {i+1}'))
-
-fig_simulation.update_layout(
-    title='Portfolio Value Simulation',
-    xaxis_title='Trading Days',
-    yaxis_title='Portfolio Value',
-)
-
-st.plotly_chart(fig_simulation)
-
-
-# In[81]:
-
-
-st.subheader("Interpreting Results and Making Decisions")
-st.write("The simulation provides a range of potential outcomes for your portfolio value over the specified time horizon.")
-st.write("Here's how you can use the results:")
-st.markdown("- **Analyze the Range**: Examine the spread of potential portfolio values. A wider spread indicates higher risk.")
-st.markdown("- **Identify Worst-Case Scenarios**: Look at the lower percentiles (e.g., 5th or 10th percentile) to identify potential worst-case scenarios.")
-st.markdown("- **Plan for Uncertainty**: Use the insights to make informed decisions about your investment strategy, asset allocation, and risk management.")
-
-
-# In[52]:
-
-
-warning_text = "<span style='color:red;'>Bias Warning</span>"
-
-# Display the warning text
-st.markdown(warning_text, unsafe_allow_html=True)
-
-with st.expander("About Cognitive Bias"):
-    st.write("""
-    **Overreaction to Extreme Data Alert:** 📈📉
-
-    When you encounter extreme scenarios in a Monte Carlo simulation or financial analysis, it can evoke intense emotional responses. These outliers may trigger fear or greed, leading to impulsive decisions that deviate from your usual risk tolerance.
-
-    For instance, in a Monte Carlo simulation, if you come across a rare scenario where investments could potentially lose a significant portion of their value, it might induce panic. In such moments, there's a risk of making hasty decisions, like selling investments hastily.
-
-    Remember that extreme scenarios are often outliers and not representative of the norm. Maintain a rational and long-term perspective, and avoid letting momentary emotions guide your actions. Strategic and well-informed decisions lead to better financial outcomes." 🧠💼
-""")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
