173286d58dc73e114b862924fc5cee2fad2c3db2
XenosHu <121327330+XenosHu@users.noreply.github.com>
2024-01-11 01:45:13 -0500

    Update Final_Code.py

--- Final_Code.py
+++ Final_Code.py
@@ -427,17 +427,17 @@ def get_googlenews(keyword):
 st.subheader(f"{selected_stock_name}({selected_stock}) Top News")
 
 news_url = get_googlenews(selected_stock_name)
-news_url_df = pd.DataFrame(news_url)
-# if news_url!={}:
-#     for d in news_url:   
-#         st.markdown(f"[{d['name']}]({d['url']})")
-#         st.write(f"Published Date: {d['date']}")
-#         sentiment_score = d['sentiment']
-#         sentiment_color = "green" if sentiment_score > 0 else "red" if sentiment_score < 0 else "grey"
-#         st.write("Sentiment Score:", f"<font color='{sentiment_color}'>{sentiment_score}</font>", unsafe_allow_html=True)
-#         st.write("---")  # Separator
-# else:
-#     st.write("No news found for the selected stock.")
+# news_url_df = pd.DataFrame(news_url)
+if news_url!={}:
+    for d in news_url:   
+        st.markdown(f"[{d['name']}]({d['url']})")
+        st.write(f"Published Date: {d['date']}")
+        sentiment_score = d['sentiment']
+        sentiment_color = "green" if sentiment_score > 0 else "red" if sentiment_score < 0 else "grey"
+        st.write("Sentiment Score:", f"<font color='{sentiment_color}'>{sentiment_score}</font>", unsafe_allow_html=True)
+        st.write("---")  # Separator
+else:
+    st.write("No news found for the selected stock.")
 
     
 def get_rdcontent(ul):
